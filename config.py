a35a1bfde1daeaf4be3874ca5b9626b8d4b59029
makchunwah <makchunwah99@gmail.com>
2024-09-19 14:42:59 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cc7pfehrhhho.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "makchunwah-employee"
+customregion = "us-east-1a"
+
